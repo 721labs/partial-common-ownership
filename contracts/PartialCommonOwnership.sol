c70476c7da0064feaf3d47793f47df5d3a5911a8
Will Holley <will@willholley.com>
2021-09-05 15:02:54 -0400

    Adds contract skeleton

---
+++ contracts/PartialCommonOwnership.sol
@@ -0,0 +1,12 @@
+// contracts/PartialCommonOwnership.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "./interfaces/IPartialCommonOwnership.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+/// @title PartialCommonOwnership
+contract PartialCommonOwnership is IPartialCommonOwnership {
+
+}
---
+++ contracts/interfaces/IPartialCommonOwnership.sol
@@ -0,0 +1,5 @@
+// contracts/interfaces/IPartialCommonOwnership.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface IPartialCommonOwnership {}
