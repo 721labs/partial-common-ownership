f9cad75ecf42b46ab3175bc74a3673b75ab6a017
Will Holley <will@willholley.com>
2021-10-06 17:43:25 -0400

    Adds DSMath

---
+++ contracts/utils/DSMath.sol
@@ -0,0 +1,66 @@
+/// contracts/utils/DSMath.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+/// @title DSMath
+/// @author DappHubb (https://dapp.tools/)
+/// @dev Trims unused functions and converts original contract to a library.
+library DSMath {
+  function add(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    require((z = x + y) >= x, "ds-math-add-overflow");
+  }
+
+  function mul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    require(y == 0 || (z = x * y) / y == x, "ds-math-mul-overflow");
+  }
+
+  uint256 constant WAD = 10**18;
+  uint256 constant RAY = 10**27;
+
+  //rounds to zero if x*y < WAD / 2
+  function wmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    z = add(mul(x, y), WAD / 2) / WAD;
+  }
+
+  //rounds to zero if x*y < WAD / 2
+  function rmul(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    z = add(mul(x, y), RAY / 2) / RAY;
+  }
+
+  //rounds to zero if x*y < WAD / 2
+  function wdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    z = add(mul(x, WAD), y / 2) / y;
+  }
+
+  //rounds to zero if x*y < RAY / 2
+  function rdiv(uint256 x, uint256 y) internal pure returns (uint256 z) {
+    z = add(mul(x, RAY), y / 2) / y;
+  }
+
+  // This famous algorithm is called "exponentiation by squaring"
+  // and calculates x^n with x as fixed-point and n as regular unsigned.
+  //
+  // It's O(log n), instead of O(n) for naive repeated multiplication.
+  //
+  // These facts are why it works:
+  //
+  //  If n is even, then x^n = (x^2)^(n/2).
+  //  If n is odd,  then x^n = x * x^(n-1),
+  //   and applying the equation for even x gives
+  //    x^n = x * (x^2)^((n-1) / 2).
+  //
+  //  Also, EVM division is flooring and
+  //    floor[(n-1) / 2] = floor[n / 2].
+  //
+  function rpow(uint256 x, uint256 n) internal pure returns (uint256 z) {
+    z = n % 2 != 0 ? x : RAY;
+
+    for (n /= 2; n != 0; n /= 2) {
+      x = rmul(x, x);
+
+      if (n % 2 != 0) {
+        z = rmul(z, x);
+      }
+    }
+  }
+}
