83e5a94a2676867ce4b5a99d1a00b589455bff4b
Will Holley <will@willholley.com>
2021-11-05 13:57:33 -0400

    Adds contract skeleton

---
+++ contracts/token/PartialCommonOwnership1155.sol
@@ -0,0 +1,9 @@
+// contracts/token/PartialCommonOwnership1155.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract PartialCommonOwnership1155 is ERC1155 {
+  constructor() {}
+}
