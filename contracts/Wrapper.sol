dc8903a9c1cd5d43c1d2ab1dbd1da3d507af5105
vicsn@users.noreply.github.com <vicsn@users.noreply.github.com>
2022-01-29 12:31:50 +0100

    Remove currently unused virtual's

--- contracts/Wrapper.sol
+++ contracts/Wrapper.sol
@@ -91,7 +91,7 @@ contract Wrapper is PartialCommonOwnership721 {
   /// @param tokenId See IERC721
   function tokenURI(
     uint256 tokenId
-  ) public view virtual override returns (string memory) { // TODO: why virtual?
+  ) public view override returns (string memory) {
     require(
       _exists(tokenId),
       "ERC721Metadata: URI query for nonexistent token"
