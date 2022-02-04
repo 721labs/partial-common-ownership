1e67ef2e9cea3d3eb90bcdd47c5481dfb979d58c
Will Holley <will@willholley.com>
2022-02-04 18:52:35 -0500

    Each test token has own tax rate & collection freq

--- contracts/test/TestPCO721Token.sol
+++ contracts/test/TestPCO721Token.sol
@@ -10,23 +10,24 @@ contract TestPCO721Token is PartialCommonOwnership721 {
   constructor(
     string memory _name,
     string memory _symbol,
-    address payable _beneficiary,
-    uint256 _taxRate,
-    uint256 _taxationPeriod
+    address payable _beneficiary
   ) PartialCommonOwnership721(_name, _symbol) {
     _safeMint(address(this), 1);
     _setBeneficiary(1, _beneficiary);
-    _setTaxRate(1, _taxRate);
-    _setTaxPeriod(1, _taxationPeriod);
+    // 5% Quarterly
+    _setTaxRate(1, 50000000000);
+    _setTaxPeriod(1, 90);
 
     _safeMint(address(this), 2);
     _setBeneficiary(2, _beneficiary);
-    _setTaxRate(2, _taxRate);
-    _setTaxPeriod(2, _taxationPeriod);
+    // 100% Monthly
+    _setTaxRate(2, 1000000000000);
+    _setTaxPeriod(2, 30);
 
     _safeMint(address(this), 3);
     _setBeneficiary(3, _beneficiary);
-    _setTaxRate(3, _taxRate);
-    _setTaxPeriod(3, _taxationPeriod);
+    // 100% Annually
+    _setTaxRate(3, 1000000000000);
+    _setTaxPeriod(3, 365);
   }
 }
