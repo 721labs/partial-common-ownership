126944378c955e86be335aaefd1f4d53704f885e
Will Holley <will@willholley.com>
2022-02-07 08:55:03 -0500

    Adds `#_backdatedForeclosureTime` to Foreclosure module

--- contracts/token/PartialCommonOwnership721.sol
+++ contracts/token/PartialCommonOwnership721.sol
@@ -446,27 +446,6 @@ contract PartialCommonOwnership721 is
   }
 
   //////////////////////////////
-  /// Prviate Getters
-  //////////////////////////////
-
-  /// @notice Returns the time when tax owed initially exceeded deposits.
-  /// @dev last collected time + ((time_elapsed * deposit) / owed)
-  /// @dev Returns within +/- 2s of previous values due to Solidity rounding
-  /// down integer division without regard for significant digits, which produces
-  /// variable results e.g. `599.9999999999851` becomes `599`.
-  /// @param tokenId_ ID of token requesting
-  /// @return Unix timestamp
-  function _backdatedForeclosureTime(uint256 tokenId_)
-    private
-    view
-    returns (uint256)
-  {
-    uint256 last = lastCollectionTimeOf(tokenId_);
-    uint256 timeElapsed = block.timestamp - last;
-    return last + ((timeElapsed * depositOf(tokenId_)) / _taxOwed(tokenId_));
-  }
-
-  //////////////////////////////
   /// ERC721 Overrides
   //////////////////////////////
 
--- contracts/token/modules/Foreclosure.sol
+++ contracts/token/modules/Foreclosure.sol
@@ -20,4 +20,25 @@ abstract contract Foreclosure is IForeclosure, Taxation, Deposit {
       return false;
     }
   }
+
+  //////////////////////////////
+  /// Internal Getters
+  //////////////////////////////
+
+  /// @notice Returns the time when tax owed initially exceeded deposits.
+  /// @dev last collected time + ((time_elapsed * deposit) / owed)
+  /// @dev Returns within +/- 2s of previous values due to Solidity rounding
+  /// down integer division without regard for significant digits, which produces
+  /// variable results e.g. `599.9999999999851` becomes `599`.
+  /// @param tokenId_ ID of token requesting
+  /// @return Unix timestamp
+  function _backdatedForeclosureTime(uint256 tokenId_)
+    internal
+    view
+    returns (uint256)
+  {
+    uint256 last = lastCollectionTimeOf(tokenId_);
+    uint256 timeElapsed = block.timestamp - last;
+    return last + ((timeElapsed * depositOf(tokenId_)) / _taxOwed(tokenId_));
+  }
 }
