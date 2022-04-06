e28746c65c419bcc1fe0adc4bc8b41745c6ee2a8
Will Holley <will@willholley.com>
2022-04-05 22:59:46 -0400

    Adds Solidity Coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
 import "hardhat-gas-reporter";
+import "solidity-coverage";
 
 import path from "path";
 import dotenv from "dotenv";
