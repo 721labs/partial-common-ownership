bcc2c5d174094dc52e75f160e3e72a2b95621eb7
Will Holley <will@willholley.com>
2022-04-10 18:11:14 -0400

    Updates installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,3 +5,14 @@ nvm use
 
 echo "\n1. Install Dependencies"
 yarn
+
+echo "\n2. Installing Foundry"
+curl -L https://foundry.paradigm.xyz | bash
+# not sure which profile you're using, so we source a few.
+source ~/.zshrc
+source ~/.profile
+source ~/.bashrc
+foundryup
+forge --version
+cast --version
+nn
